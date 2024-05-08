560d2b641a0e6f0c056ea6131890ffd27a1084c8
Ace <aceshyjan@gmail.com>
2024-05-08 20:16:09 +0100

    .

--- TraceRouteCode/traceroute_csv_to_graph.py
+++ TraceRouteCode/traceroute_csv_to_graph.py
@@ -9,7 +9,6 @@ path = folder+'/traceroute.csv'
 reader_asn = geoip2.database.Reader(folder+'/GeoLite2-ASN.mmdb')
 reader_city = geoip2.database.Reader(folder+'/GeoLite2-City.mmdb')
 
-custom_icon_path = folder+'/custom_icon.png'
 
 ip_addresses = {
     "www.fmprc.gov.cn": {}, 
@@ -36,12 +35,12 @@ def get_coordinates(ip_address):
         longitude = response.location.longitude
         latitude = response.location.latitude
         location = response.city.name
-        ip_lookup[ip_address] = (longitude, latitude, location)
+        ip_lookup[ip_address] = (latitude, longitude, location)
         
         
     except geoip2.errors.AddressNotFoundError:
         pass
-    return longitude, latitude, location
+    return latitude, longitude, location
 
 def load_csv(path):
     return pd.read_csv(path)
@@ -59,7 +58,7 @@ def main(path):
         for i, row in group.iterrows():
             
             # Get the coordinates of the IP address
-            longitude, latitude, location = ip_lookup.get(row['IP'], get_coordinates(row['IP']))
+            latitude, longitude, location = ip_lookup.get(row['IP'], get_coordinates(row['IP']))
                 
             if longitude is not None and latitude is not None:
                 folium.CircleMarker(
@@ -77,7 +76,7 @@ def main(path):
         folium.PolyLine(coordinates, color="red", weight=2.5, opacity=1).add_to(m)
                 
         # address = address.split('.')[1]
-        file_name = f'{file}_{address}.html'
+        file_name = f'{address}_{file}.html'
         m.save(f'{save_folder}/{file_name}')
         
         
